574dbde406b81e7ee4ecfa9202353ab4cd98f136
Martin Ezequiel Saponara <Martin-Ezequiel.Saponara@endava.com>
2023-04-18 15:19:23 -0300

    code cleanup

--- src/CodingChallenge_Nextech/ClientApp/src/app/app.module.ts
+++ src/CodingChallenge_Nextech/ClientApp/src/app/app.module.ts
@@ -4,19 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,    
     FetchDataComponent
   ],
   imports: [
@@ -25,9 +20,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     NgxPaginationModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: FetchDataComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
